7578ac867879c2bf289ccb2aa9b3dc1904a61af8
qkrtjdrb <qkrtjdrb321@naver.com>
2024-09-04 15:31:04 +0900

    git

---
+++ git.py
@@ -0,0 +1,13 @@
+for i in range(1,6):
+    print('*'*i)
+
+for i in range(1,6):
+    print(' '*(5-i)+'*'*i)
+
+n=3
+for i in range(n):
+    print(' '*(-i+2)+'*'*(2*i+1))
+
+for i in range(n, 5):
+    print(' '*(1 + (i % 3))+'*'*(3 - 2*(i % 3)))
+
